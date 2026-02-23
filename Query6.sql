55d68eb0930cce8f0b6ad9308c3a618b3bdd02a2
ZainR525 <rizvi.zain@northeastern.edu>
2026-02-23 12:12:25 -0800

    Add query to select distinct artist names for playlists

--- Query6.sql
+++ Query6.sql
@@ -1,1 +1,9 @@
-
+SELECT DISTINCT artists.Name
+FROM artists
+JOIN albums ON artists.ArtistId = albums.ArtistId
+JOIN tracks ON albums.AlbumId = tracks.AlbumId
+JOIN media_types ON tracks.MediaTypeId = media_types.MediaTypeId
+JOIN playlist_track ON tracks.TrackId = playlist_track.TrackId
+JOIN playlists ON playlist_track.PlaylistId = playlists.PlaylistId
+WHERE media_types.Name LIKE '%MPEG%'
+  AND playlists.Name IN ('Brazilian Music', 'Grunge');
