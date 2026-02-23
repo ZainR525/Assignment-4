95b17050cd94e18a90226ebbb5827e51ebd7ed68
ZainR525 <rizvi.zain@northeastern.edu>
2026-02-23 12:13:13 -0800

    Update Query7.sql

--- Query7.sql
+++ Query7.sql
@@ -1,1 +1,11 @@
-
+SELECT COUNT(*) AS ArtistsWith10OrMoreMPEGTracks
+FROM (
+    SELECT artists.ArtistId
+    FROM artists
+    JOIN albums ON artists.ArtistId = albums.ArtistId
+    JOIN tracks ON albums.AlbumId = tracks.AlbumId
+    JOIN media_types ON tracks.MediaTypeId = media_types.MediaTypeId
+    WHERE media_types.Name LIKE '%MPEG%'
+    GROUP BY artists.ArtistId
+    HAVING COUNT(tracks.TrackId) >= 10
+);
