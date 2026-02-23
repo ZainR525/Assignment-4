d346b016ba2beb8069e7bca7672f400b1f58851e
ZainR525 <rizvi.zain@northeastern.edu>
2026-02-23 12:22:02 -0800

    Refactor SQL query to use implicit join syntax

--- Query1.sql
+++ Query1.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT c.LastName, c.Email
-FROM customers c
-JOIN invoices i ON c.CustomerId = i.CustomerId
-WHERE c.LastName IS NOT NULL AND c.Email IS NOT NULL;
+FROM customers c, invoices i
+WHERE c.CustomerId = i.CustomerId
+AND c.LastName IS NOT NULL
+AND c.Email IS NOT NULL;
