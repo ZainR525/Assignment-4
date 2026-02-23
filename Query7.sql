8816e28fd7ae5f97c02c05fabff2e941f76d7139
ZainR525 <rizvi.zain@northeastern.edu>
2026-02-23 12:24:06 -0800

    Refactor SQL query to use implicit joins

--- Query7.sql
+++ Query7.sql
@@ -1,11 +1,11 @@
 SELECT COUNT(*) AS ArtistsWith10OrMoreMPEGTracks
 FROM (
-    SELECT artists.ArtistId
-    FROM artists
-    JOIN albums ON artists.ArtistId = albums.ArtistId
-    JOIN tracks ON albums.AlbumId = tracks.AlbumId
-    JOIN media_types ON tracks.MediaTypeId = media_types.MediaTypeId
-    WHERE media_types.Name LIKE '%MPEG%'
-    GROUP BY artists.ArtistId
-    HAVING COUNT(tracks.TrackId) >= 10
+    SELECT ar.ArtistId
+    FROM artists ar, albums al, tracks t, media_types mt
+    WHERE ar.ArtistId = al.ArtistId
+    AND al.AlbumId = t.AlbumId
+    AND t.MediaTypeId = mt.MediaTypeId
+    AND mt.Name LIKE '%MPEG%'
+    GROUP BY ar.ArtistId
+    HAVING COUNT(t.TrackId) >= 10
 );
