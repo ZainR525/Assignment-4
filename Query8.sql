708447aeebbf838fa3f436544a67ab517f7196e6
ZainR525 <rizvi.zain@northeastern.edu>
2026-02-23 12:13:41 -0800

    Update Query8.sql

--- Query8.sql
+++ Query8.sql
@@ -1,1 +1,10 @@
+SELECT 
+    playlists.PlaylistId,
+    playlists.Name,
+    ROUND(SUM(tracks.Milliseconds) / 3600000.0, 2) AS LengthInHours
+FROM playlists
+JOIN playlist_track ON playlists.PlaylistId = playlist_track.PlaylistId
+JOIN tracks ON playlist_track.TrackId = tracks.TrackId
+GROUP BY playlists.PlaylistId, playlists.Name
+HAVING LengthInHours > 2;
 
