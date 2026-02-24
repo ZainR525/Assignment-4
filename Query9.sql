fca62a6e868b84baf5f7e2ed4c5c99ea48792a57
ZainR525 <rizvi.zain@northeastern.edu>
2026-02-23 17:13:34 -0800

    Update Query9.sql

--- Query9.sql
+++ Query9.sql
@@ -1,6 +1,14 @@
-SELECT ar.Name, COUNT(t.TrackId) AS TotalTracks,
-    ROW_NUMBER() OVER (ORDER BY COUNT(t.TrackId) DESC) AS ArtistRank
-FROM artists ar, albums al, tracks t
-WHERE ar.ArtistId = al.ArtistId
-AND al.AlbumId = t.AlbumId
-GROUP BY ar.ArtistId;
+WITH ArtistGenreCount AS (
+    SELECT ar.Name AS Artist, g.Name AS Genre, COUNT(t.TrackId) AS TrackCount
+    FROM artists ar, albums al, tracks t, genres g
+    WHERE ar.ArtistId = al.ArtistId
+    AND al.AlbumId = t.AlbumId
+    AND t.GenreId = g.GenreId
+    GROUP BY ar.Name, g.Name
+),
+Ranking AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY Genre ORDER BY TrackCount DESC) AS row
+    FROM ArtistGenreCount
+)
+SELECT * FROM Ranking
+WHERE row <= 3;
